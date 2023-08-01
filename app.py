f99d53476005e70b8f9d84bde084cfdd92ec06a4
Yuzhou Shen <shenyuzhou@live.com>
2023-08-01 11:35:16 +0800

    Final

--- app.py
+++ app.py
@@ -1,6 +1,84 @@
 from flask import Flask
+import psycopg2
+
+
 app = Flask(__name__)
+dbURL = "postgres://lab_11_yzs_user:AQdln82rPa59QV1aD3OW1nUJtZOrEI3k@dpg-cj47h12ip7vuasjknpg0-a/lab_11_yzs"
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/db_test')
+def testing():
+    conn=psycopg2.connect(dbURL)
+    conn.close()
+    return "Database Connection Successful!"
+
+
+@app.route('/db_create')
+def creating():
+    conn=psycopg2.connect(dbURL)
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+    ''')
+    conn.commit()
+    cur.close()
+    conn.close()
+    return "Basketball Table Successfully Created!"
+
+@app.route('/db_insert')
+def inserting():
+    conn=psycopg2.connect(dbURL)
+    cur = conn.cursor()
+    cur.execute('''
+    INSERT INTO Basketball (First, Last, City, Name, Number)
+    Values
+    ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+    ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+    ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+    ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+    ''')
+    conn.commit()
+    cur.close()
+    conn.close()
+    return "Basketball Table Populated"
+
+@app.route('/db_select')
+def selecting():
+    conn=psycopg2.connect(dbURL)
+    cur = conn.cursor()
+    cur.execute('''
+    SELECT * FROM Basketball;
+    ''')
+    records = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    response = ""
+    response += "<table>"
+    for player in records:
+        response += "<tr>"
+        for info in player:
+            response += f"<td>{info}</td>"
+        response += "</tr>"
+    response += "</table>"
+    return response
+
+@app.route('/db_drop')
+def dropping():
+    conn=psycopg2.connect(dbURL)
+    cur = conn.cursor()
+    cur.execute('''
+        DROP TABLE Basketball;
+    ''')
+    cur.close()
+    conn.close()
+    return "Basketball Table Dropped"
